4ed23d81cfaf61b225863e3463238f37f3a95e74
Name <aguiar_moises@hotmail.com>
2023-04-24 18:50:57 -0400

    commit

--- src/pages/dev/index.tsx
+++ src/pages/dev/index.tsx
@@ -1,9 +1,41 @@
-const index = () => {
+import useFetch from "@/hooks/usehooks-ts";
+import { GetStaticProps, NextPage } from "next";
+import pokeApi from "../api/Pokemons";
+
+export interface Pokemons {
+  count: number;
+  next: string;
+  previous: string;
+  results: Result[];
+}
+
+export interface Result {
+  name: string;
+  url: string;
+}
+
+const index: NextPage = (props: any) => {
+  console.log(props);
+
   return (
     <>
+      {}
       <h1>Hola</h1>
+      <ul>
+        <li>{pokemons.map}</li>
+      </ul>
     </>
   );
+};
+
+export const getStaticProps: GetStaticProps = async (props: any) => {
+  const { data } = await pokeApi.get("/pokemon?limit=151");
+
+  return {
+    props: {
+      pokemons: data.results,
+    }, // will be passed to the page component as props
+  };
 };
 
 export default index;
